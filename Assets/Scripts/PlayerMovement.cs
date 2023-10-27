06c113aa6671f20d39689f7ec02e4754af2eb5a3
Fedor Ptitsyn <fedya.ptitsyn@mail.ru>
2023-10-27 15:57:26 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private float movementX;
+
+    private void Update()
+    {
+        HandlePlayerMovement();
+    }
+
+    void HandlePlayerMovement()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movementX, 0f, 0f) * (moveSpeed * Time.deltaTime);
+    }
+}
